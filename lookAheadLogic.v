902e4d93caee27b91124137b2128953cc85543ed
Jayant <jayant91@users.noreply.github.com>
2017-04-17 23:31:30 -0500

    Wallace8 somewhat working

--- lookAheadLogic.v
+++ lookAheadLogic.v
@@ -1,30 +1,30 @@
-module lookAheadLogic(cout1, cout2, cout3, gout, pout, ain0, bin0, ain1, bin1, ain2, bin2, ain3, bin3, cin0);
-	
-	input ain0, ain1, ain2, ain3;
-	input bin0, bin1, bin2, bin3;
-	input cin0;
+module lookAheadLogic(cout, gout, pout, ain, bin, cin0);
+	output [2:0] cout;
+ 	output gout;
+	output pout;
 
-	output cout1, cout2, cout3;
-	output gout, pout;
+	input [3:0] ain;
+	input [3:0] bin;
+	input cin0;
 
 	wire p0, p1, p2, p3;
 	wire g0, g1, g2, g3;
 	
-	assign g0 	= ain0 && bin0;
-	assign p0	= ain0 || bin0;
+	assign g0 	= ain[0] && bin[0];
+	assign p0	= ain[0] || bin[0];
 
-	assign g1 	= ain1 && bin1;
-	assign p1	= ain1 || bin1;
+	assign g1 	= ain[1] && bin[1];
+	assign p1	= ain[1] || bin[1];
 
-	assign g2 	= ain2 && bin2;
-	assign p2	= ain2 || bin2;
+	assign g2 	= ain[2] && bin[2];
+	assign p2	= ain[2] || bin[2];
 
-	assign g3 	= ain3 && bin3;
-	assign p3	= ain3 || bin3;
+	assign g3 	= ain[3] && bin[3];
+	assign p3	= ain[3] || bin[3];
 
-	assign cout1	= g0 || (p0 && cin0);
-	assign cout2	= g1 || (p1 && g0) || (p1 && p0 && cin0);
-	assign cout3	= g2 || (p2 && g1) || (p2 && p1 && g0) || (p2 && p1 && p0 && cin0);
+	assign cout[0]	= g0 || (p0 && cin0);
+	assign cout[1]	= g1 || (p1 && g0) || (p1 && p0 && cin0);
+	assign cout[2]	= g2 || (p2 && g1) || (p2 && p1 && g0) || (p2 && p1 && p0 && cin0);
 
 	assign gout	= g3 || (g2 && p3) || (p3 && p2 && g1) || (p3 && p2 && p1 && g0);
 	assign pout	= p3 && p2 && p1 && p0;
